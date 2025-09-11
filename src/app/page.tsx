e2654772d13fda292746f35a9f67d22e728de159
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-09-11 19:56:47 +0530

    use font sans (#5)
    * use font sans
    
    * preview url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import imageAstrophysics from "@/assets/astrophysics-spectrum.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center mx-auto max-w-screen-md p-4">
+    <div className="flex flex-col items-center justify-center mx-auto max-w-screen-md p-4 font-sans">
       <div className="flex flex-col gap-4 w-full">
         <Button>hello world</Button>
         <Button variant="destructive">hello world</Button>
