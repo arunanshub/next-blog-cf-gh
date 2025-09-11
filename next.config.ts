13821da5c11573437127547676c97ed642119e7e
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-09-11 14:54:22 +0530

    small optimizations (#2)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    inlineCss: true,
+    webpackMemoryOptimizations: true,
+  }
 };
 
 export default nextConfig;
