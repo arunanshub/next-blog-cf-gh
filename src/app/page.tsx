684b7bf0a2295dc015cf59c221bc086774a9a8af
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2025-09-11 17:22:26 +0530

    add card (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-dvh">
       <Button>hello world</Button>
+      <Button variant="destructive">hello world</Button>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>hello world</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <p>hello world</p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
