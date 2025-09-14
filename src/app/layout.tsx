8533b279613c98573e9eec5a5e0c42f8ee87e4a2
Arunanshu Biswas <mydellpc07@gmail.com>
2025-09-14 16:43:01 +0530

    rich colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Providers>
           {children}
-          <Toaster />
+          <Toaster richColors closeButton />
         </Providers>
       </body>
     </html>
